ed06c11ba80c791f32764a02013a5d3164fce40b
thebiewza <ratchanontip@gmail.com>
2021-03-28 23:25:03 +0700

    แก้นิดหน่อย

--- No3.cs
+++ No3.cs
@@ -7,71 +7,6 @@ namespace MidTerm2_no._3_
         static void Main(string[] args)
         {
 
-            //while (BoxT1 > 0 || BoxT2 > 0 || BoxT3 > 0)
-            //{
-
-            //    int chooseTankA;
-            //    Console.Write("PlayerA, Choose the tank 1-3 : ");
-            //    chooseTankA = int.Parse(Console.ReadLine());
-
-            //    int boxA;
-            //    Console.Write("PlayerA, Choose the box out : ");
-            //    boxA = int.Parse(Console.ReadLine());
-
-            //    switch (chooseTankA)
-            //    {
-            //        case (1):
-            //            BoxT1 = BoxT1 - boxA;
-            //            break;
-            //        case (2):
-            //            BoxT2 = BoxT2 - boxA;
-            //            break;
-            //        case (3):
-            //            BoxT3 = BoxT3 - boxA;
-            //            break;
-            //    }
-            //    Console.WriteLine("Box1 : {0} Box2 : {1} Box3 : {2}", BoxT1, BoxT2, BoxT3);
-            //    Console.WriteLine();
-
-            //    int chooseTankB;
-            //    Console.Write("PlayerB, Choose the tank 1-3: ");
-            //    chooseTankB = int.Parse(Console.ReadLine());
-
-            //    int boxB;
-            //    Console.Write("PlayerB, Choose the box out : ");
-            //    boxB = int.Parse(Console.ReadLine());
-
-            //    switch (chooseTankB)
-            //    {
-            //        case (1):
-            //            BoxT1 = BoxT1 - boxB;
-            //            break;
-            //        case (2):
-            //            BoxT2 = BoxT2 - boxB;
-            //            break;
-            //        case (3):
-            //            BoxT3 = BoxT3 - boxB;
-            //            break;
-            //    }
-            //    Console.WriteLine("Box1 : {0} Box2 : {1} Box3 : {2}", BoxT1, BoxT2, BoxT3);
-            //    Console.WriteLine();
-
-            //}
-
-            //if (turn % 2 == 0)
-            //{
-            //    Console.WriteLine("The Winner is PlayerA");
-            //}
-            //else if (turn % 2 != 0)
-            //{
-            //    Console.WriteLine("The Winner is PlayerB");
-            //}
-            //Console.WriteLine(turn);
-
-            //Console.ReadLine();
-
-            //-----------------------------------------------------------------------------------------------------
-
             int BoxT1;
             Console.Write("Input box in tank 1 : ");
             BoxT1 = int.Parse(Console.ReadLine());
@@ -101,6 +36,7 @@ namespace MidTerm2_no._3_
                 {
                     Console.WriteLine("Player B");
                 }
+
                 int chooseTank;
                 Console.Write("Choose the tank 1-3: ");
                 chooseTank = int.Parse(Console.ReadLine());
@@ -123,8 +59,6 @@ namespace MidTerm2_no._3_
                 }
                 Console.WriteLine("Box1 : {0} Box2 : {1} Box3 : {2}", BoxT1, BoxT2, BoxT3);
                 Console.WriteLine();
-
-                
 
             }
 
