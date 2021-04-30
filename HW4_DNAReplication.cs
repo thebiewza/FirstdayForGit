8ca15c2652470ab5d612ea70499b126d425ff94e
thebiewza <ratchanontip@gmail.com>
2021-04-30 11:38:49 +0700

    Homework 4 (workshop)

---
+++ HW4_DNAReplication.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void DNAReplication()
+        {
+            string DNA = Console.ReadLine();
+
+            if ((IsValidSequence(DNA)) == true)
+            {
+                Console.WriteLine("Current half DNA sequence : {0}", DNA);
+
+            }
+            else
+            {
+                Console.WriteLine("That half DNA sequence is invalid. ");
+                CheckSequence();
+            }
+
+            CheckReplicate(DNA);
+            Console.ReadLine();
+        }   
+
+        static void CheckReplicate(string DNA)
+        {
+            Console.Write("Do you want  to replicate it ? (Y/N) : ");
+            string checkReplicate = Console.ReadLine();
+                
+            switch (checkReplicate)
+            {
+                case ("Y"):
+                    Console.WriteLine("Replicated half DNA sequence : {0}", (ReplicateSeqeunce(DNA)));
+                    CheckSequence();
+                    break;
+                case ("N"):
+                    CheckSequence();
+                    break;
+                default:
+                    Console.WriteLine("Please input Y or N.");
+                    CheckReplicate(DNA);
+                    break;
+                }
+        }   
+
+        static void CheckSequence()
+        {
+            Console.Write("Do you want to process another sequence ? (Y/N) : ");
+            string checkSequence = Console.ReadLine();
+
+            switch (checkSequence)
+            {
+                case ("Y"):
+                    DNAReplication();
+                    break;
+                case ("N"):
+
+                    break;
+                default:
+                    Console.WriteLine("Please input Y or N.");
+                    CheckSequence();
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        
+        static bool IsValidSequence(string halfDNASequence)
+        {
+            foreach (char nucleotide in halfDNASequence)
+            {
+                if (!"ATCG".Contains(nucleotide))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static string ReplicateSeqeunce(string halfDNASequence)
+        {
+            string result = "";
+            foreach (char nucleotide in halfDNASequence)
+            {
+                result += "TAGC"["ATCG".IndexOf(nucleotide)];
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ HW4_Pascal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Pascaltriangle()
+        {
+            int input = int.Parse(Console.ReadLine());
+            
+            int val = 0;
+
+            if (input == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (input > 0)
+            {
+                input = input + 1;
+                for (int i = 0; i < input; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (j == 0 || i == 0)
+                        {
+                            val = 1;
+                        }
+                        else
+                        {
+                            val = val * (i - j + 1) / j;
+                        }
+                        Console.Write(val + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pascal’s triangle row number.");
+                Pascaltriangle();
+            }
+        }
+
+    }
+}
