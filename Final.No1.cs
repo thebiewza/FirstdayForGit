ed2d7a29e1a6db1a3846214c370d6f402d26297b
thebiewza <ratchanontip@gmail.com>
2021-06-13 17:18:50 +0700

    No1 & No5

---
+++ Final.No1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.No._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] input = new int[3, 3];
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    input[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                Console.WriteLine("{0} {1} {2}", input[i, 0], input[i, 1], input[i, 2]);
+            }
+
+            int replace = int.Parse(Console.ReadLine());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Final.No5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.No._5
+{
+    class Program
+    {
+        static void MyInventory()
+        {
+            int NumOfItem = int.Parse(Console.ReadLine());
+
+
+            string[] NameItem;
+            string[] TypeItem;
+
+            for (int i = 0; i < NumOfItem; i++)
+            {
+                NameItem = new string[NumOfItem];
+                TypeItem = new string[NumOfItem];
+            }
+
+            string Mode = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+
+        static void ModeToFindItem(string Mode, int num, string[] Name, string[] Type)
+        {
+            if (Mode == "ShowAll")
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    ModeToFindItem(Mode, num, Name, Type);
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
